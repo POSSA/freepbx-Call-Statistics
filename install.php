afa39d2583bb1b34b5f754ed86f48981e476fc67
unknown <lorne_g@TRU-D-2009-0909.mcw.com>
2012-12-12 13:26:10 -0400

    undo previous commit for #4

--- install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!(extension_loaded('gd') && function_exists('gd_info'))) {
-    echo "Required dependency is missing.  GD extensions for PHP must be installed for this module to operate.";
-	die;
-}
-	
-?>
