a5a7cfad7402987bf4e2f3afa3e15babd9cc41cc
Andrew Nagy <andrew.nagy@the159.com>
2014-03-13 13:19:10 -0400

    Turn into a proper FreePBX module, No iframes

--- page.extbreakdown.php
+++ page.extbreakdown.php
@@ -1,16 +1,18 @@
 <?php
 // check to see if user has automatic updates enabled in FreePBX settings
-$cm =& cronmanager::create($db);
-$online_updates = $cm->updates_enabled() ? true : false;
+if($quietmode && $_REQUEST['ext_graph']) {
+	include_once(dirname(__FILE__).'/extbreakdown/ext_graph.php');
+} else {
+	$cm =& cronmanager::create($db);
+	$online_updates = $cm->updates_enabled() ? true : false;
 
-// check dev site to see if new version of module is available
-if ($online_updates && $foo = callstatistics_vercheck()) {
-	echo "<br>A <b>new version of this module is available</b> from the <a target='_blank' href='http://pbxossa.org'>PBX Open Source Software Alliance</a><br>";
+	// check dev site to see if new version of module is available
+	if ($online_updates && $foo = callstatistics_vercheck()) {
+		echo "<br>A <b>new version of this module is available</b> from the <a target='_blank' href='http://pbxossa.org'>PBX Open Source Software Alliance</a><br>";
 	}
+	include_once(dirname(__FILE__).'/extbreakdown/extbreakdown.php');
 
-?><iframe width=100% height=600px src="modules/callstatistics/extbreakdown/extbreakdown.php" />
-</iframe>
-<?php
-$module_local = callstatistics_xml2array("modules/callstatistics/module.xml");
-echo $module_local[module][name]." ver: ".$module_local[module][version];
+	$module_local = callstatistics_xml2array("modules/callstatistics/module.xml");
+	echo $module_local[module][name]." ver: ".$module_local[module][version];
+}
 ?>
